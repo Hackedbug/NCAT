4c41384cc939e1fd9e4359199178aed58c8d3c3e
Hackedbug <ahmedyinusa2004@gmail.com>
2024-10-24 12:35:14 +0100

    Updated test.py

--- test.py
+++ test.py
@@ -1,22 +1,39 @@
-#it is used to able to assign a meaning to a particular element in the program such as abbreviations
-month = { #a dictionary for the abbreviations of the months of the year
-	"Jan" and "jan": "January",
-	"Feb" and "feb": "February",
-	"Mar" and "mar": "March",
-	"Apr" and "apr": "April",
-	"May" and "may": "May",
-	"Jun" and "jun": "June",
-	"Jul" and "jul": "July",
-	"Aug" and "aug": "August",
-	"Sep" and "sep": "September",
-	"Oct" and "oct": "October",
-	"Nov" and "nov": "November",
-	"Dec" and "dec": "December",
+#This is a currency converter program converting USD to different currencies
+rate_from_USD = { # dictionary containing exchange rates from different currencies to USD
+    'USD': 1.0,
+    'EUR': 0.85,
+    'JPY': 110.0,
+    'GBP': 0.75,
+    'AUD': 1.35,
+    'CAD': 1.25,
+    'CHF': 0.92,
+    'CNY': 6.5,
+    'INR': 73.0,
+    'NGN': 1600.0,
 }
 
-print("This is a program to change the abbreviation of a month to the full name of the month.")
-abbr = input("Enter the abbreviation: ")
-if abbr in month: #to make the program cross-check the dictionary for the month
-    print(month[abbr]) #to get the exact name assigned to Jan
-else: #if the abbr provided by the user is not among the assigned
-    print(abbr + " is not a month abbreviation.")
+amount = float(input("Enter the amount to be converted: ")) # the quantity of USD to be converted to other currencies
+from_currency = input("Enter the currency you're converting from: ").upper() #the currency to be converted
+to_currency = input("Enter the currency you're converting to: ").upper() #the currency to convert to
+
+if to_currency and from_currency in rate_from_USD: #if statement to check if both values are among the dictionaries before executing
+    if to_currency == "EUR":
+        print (amount / rate_from_USD.get(to_currency))
+    elif to_currency == 'JPY':
+        print (amount * rate_from_USD.get(to_currency))
+    elif to_currency == 'GBP':
+        print (amount / rate_from_USD.get(to_currency))
+    elif to_currency == 'AUD':
+        print (amount * rate_from_USD.get(to_currency))
+    elif to_currency == 'CAD':
+        print (amount * rate_from_USD.get(to_currency))
+    elif to_currency == 'CHF':
+        print (amount / rate_from_USD.get(to_currency))
+    elif to_currency == 'CNY':
+        print (amount * rate_from_USD.get(to_currency))
+    elif to_currency == 'INR':
+        print (amount * rate_from_USD.get(to_currency))
+    else:
+        print (amount * rate_from_USD.get(to_currency))
+else: #the statement if the to currency is not among the dictionary
+    print(to_currency + " is not a valid currency in the world.")
